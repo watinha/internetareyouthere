9f7bdd3e6e891826a2cc9111ec4fd8e9c55a6f87
Willian Massami Watanabe <watanabe_willian@yahoo.com>
2013-10-16 19:32:55 -0300

    now with bad path

--- internetareyouthere.sh
+++ internetareyouthere.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 A=`curl http://www.google.com`
 if [ "$?" -eq 0 ]; then
-    echo "OK"
+    echo "Yes there is Internet..."
+else
+    echo "----------------------------------------"
+    echo "|  Noooooo, we have no Internets... :( |"
+    echo "----------------------------------------"
 fi
