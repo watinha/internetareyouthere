dc99c3eccafcdf1ef5d792795ac5d3e6c97b0f01
Willian Massami Watanabe <watanabe_willian@yahoo.com>
2013-10-16 19:42:50 -0300

    now loging Internet connection in a log file

--- internetareyouthere.sh
+++ internetareyouthere.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
-A=`curl http://www.google.com`
+LOG="/var/log/internetwhereareyou.log"
+A=`curl --connect-timeout 3 http://www.google.com`
 if [ "$?" -eq 0 ]; then
     date=`date`
-    echo "$date : Yes there is Internet..."
+    echo "$date : Yes there is Internet..." >> $LOG
 else
-    echo ""
+    echo "" >> $LOG
     date=`date`
-    echo "$date : Internets where are you???"
-    echo "----------------------------------------"
-    echo "|  Noooooo, we have no Internets... :( |"
-    echo "----------------------------------------"
-    echo ""
+    echo "$date : Internets where are you???" >> $LOG
+    echo "----------------------------------------" >> $LOG
+    echo "|  Noooooo, we have no Internets... :( |" >> $LOG
+    echo "----------------------------------------" >> $LOG
+    echo "" >> $LOG
 fi
