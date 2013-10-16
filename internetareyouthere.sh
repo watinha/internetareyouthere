69a862d9ed8b4f9d0602c9090f32a7ec98503e03
Willian Massami Watanabe <watanabe_willian@yahoo.com>
2013-10-16 20:06:16 -0300

    now more like a service

--- internetareyouthere.sh
+++ internetareyouthere.sh
@@ -1,19 +1,27 @@
 #!/bin/sh
 LOG="/var/log/internetwhereareyou.log"
-while :
-do
-    A=`curl --connect-timeout 3 http://www.google.com`
-    if [ "$?" -eq 0 ]; then
-        date=`date`
-        echo "$date : Yes there is Internet..." >> $LOG
-    else
-        echo "" >> $LOG
-        date=`date`
-        echo "$date : Internets where are you???" >> $LOG
-        echo "----------------------------------------" >> $LOG
-        echo "|  Noooooo, we have no Internets... :( |" >> $LOG
-        echo "----------------------------------------" >> $LOG
-        echo "" >> $LOG
-    fi
-    sleep 5
-done
+
+case "$1" in
+    start)
+        while :
+        do
+            A=`curl --connect-timeout 3 http://www.google.com`
+            if [ "$?" -eq 0 ]; then
+                date=`date`
+                echo "$date : Yes there is Internet..." >> $LOG
+            else
+                echo "" >> $LOG
+                date=`date`
+                echo "$date : Internets where are you???" >> $LOG
+                echo "----------------------------------------" >> $LOG
+                echo "|  Noooooo, we have no Internets... :( |" >> $LOG
+                echo "----------------------------------------" >> $LOG
+                echo "" >> $LOG
+            fi
+            sleep 5
+        done
+    ;;
+    stop)
+        killall internetareyouthere.sh
+    ;;
+esac
