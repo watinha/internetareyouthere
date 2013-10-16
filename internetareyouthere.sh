703ba2bdd3aab737392d193b20aba3063c28d8d9
Willian Massami Watanabe <watanabe_willian@yahoo.com>
2013-10-16 19:35:38 -0300

    now with time

--- internetareyouthere.sh
+++ internetareyouthere.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 A=`curl http://www.google.com`
 if [ "$?" -eq 0 ]; then
-    echo "Yes there is Internet..."
+    date=`date`
+    echo "$date : Yes there is Internet..."
 else
+    echo ""
+    date=`date`
+    echo "$date : Internets where are you???"
     echo "----------------------------------------"
     echo "|  Noooooo, we have no Internets... :( |"
     echo "----------------------------------------"
+    echo ""
 fi
